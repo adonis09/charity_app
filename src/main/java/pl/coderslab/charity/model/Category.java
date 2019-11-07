258af213751ace670b155cdce7d1d1464108dc1d
Marek Kuźmuk <marek.kuzmuk@gmail.com>
2019-11-07 22:14:14 +0100

    Category entity and repository

---
+++ src/main/java/pl/coderslab/charity/model/Category.java
@@ -0,0 +1,26 @@
+package pl.coderslab.charity.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+}
