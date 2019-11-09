2ed3faae465faa1a009bcfb38f3a8971a999f395
Marek Kuźmuk <marek.kuzmuk@gmail.com>
2019-11-09 22:00:10 +0100

    modified homeAction in HomeController, displaying institutions through model attribute

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -1,16 +1,28 @@
 package pl.coderslab.charity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.Institution;
+import pl.coderslab.charity.repository.InstitutionRepository;
+import pl.coderslab.charity.service.InstitutionService;
+
+import java.util.List;
 
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private InstitutionService institutionService;
 
     @RequestMapping("/")
     public String homeAction(Model model){
+
+        List<Institution> institutionsList = institutionService.findAll();
+        model.addAttribute("allInstitutions", institutionsList);
         return "index";
     }
+
 }
