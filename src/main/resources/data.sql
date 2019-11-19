5178f32ad82f0e3e8fce693ba44205d66cbc1b8b
Marek Kuźmuk <marek.kuzmuk@gmail.com>
2019-11-19 21:37:19 +0100

    updated data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 
-REPLACE INTO `institution` VALUES
+REPLACE INTO `institution` (id, name, description) VALUES
 (1, 'Dbam o Zdrowie', 'Pomoc dzieciom z ubogich rodzin.'),
 (2, 'Dla dzieci', 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej.'),
 (3, 'A kogo', 'Pomoc wybudzaniu dzieci ze śpiączki.'),
@@ -7,14 +7,14 @@ REPLACE INTO `institution` VALUES
 (5, 'Złota jesień', 'Pomoc dla starszych, samotnych osób.'),
 (6, 'Psia łapa', 'Wspieranie schronisk dla psów.');
 
-REPLACE INTO `category` VALUES
+REPLACE INTO `category` (id, name, description) VALUES
 (1, 'clothes-to-use', 'ubrania, które nadają się do ponownego użycia'),
 (2, 'clothes-useless', 'ubrania do wyrzucenia'),
 (3, 'toys', 'zabawki'),
 (4, 'books', 'książki'),
 (5, 'other', 'inne');
 
-REPLACE INTO `institution_category` VALUES
+REPLACE INTO `institution_category` (institution_id, category_id) VALUES
 (1, 1),
 (1, 3),
 (1, 4),
