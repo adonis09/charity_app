1c54cc1d6ef32bd599c18c40a397aee9de2bd308
Marek Kuźmuk <marek.kuzmuk@gmail.com>
2019-11-19 21:08:52 +0100

    loading data from data.sql file on runnig application

---
+++ src/main/resources/data.sql
@@ -0,0 +1,25 @@
+
+REPLACE INTO `institution` VALUES
+(1, 'Dbam o Zdrowie', 'Pomoc dzieciom z ubogich rodzin.'),
+(2, 'Dla dzieci', 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej.'),
+(3, 'A kogo', 'Pomoc wybudzaniu dzieci ze śpiączki.'),
+(4, 'Bez domu', 'Pomoc dla osób nie posiadających miejsca zamieszkania.'),
+(5, 'Złota jesień', 'Pomoc dla starszych, samotnych osób.'),
+(6, 'Psia łapa', 'Wspieranie schronisk dla psów.');
+
+REPLACE INTO `category` VALUES
+(1, 'clothes-to-use', 'ubrania, które nadają się do ponownego użycia'),
+(2, 'clothes-useless', 'ubrania do wyrzucenia'),
+(3, 'toys', 'zabawki'),
+(4, 'books', 'książki'),
+(5, 'other', 'inne');
+
+REPLACE INTO `institution_category` VALUES
+(1, 1),
+(1, 3),
+(1, 4),
+(2, 1),
+(3, 3),
+(4, 1),
+(5, 1),
+(6, 2);
